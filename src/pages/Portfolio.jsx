52615253d72f706cd849e336a848ab235abd0592
CedricKernec <cedrickernec.ck@gmail.com>
2025-11-12 16:59:14 +0100

    feat(portfolio): création de la page Portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,5 +1,31 @@
+import banner from "../assets/images/banner.jpg";
+import Container from "../components/common/Container";
+import SectionDivider from "../components/common/SectionDivider";
+import CardPorfolio from "../components/ui/CardPortfolio";
+
 export default function Portfolio() {
   return (
-    <div>Portfolio</div>
+    <>
+      <img
+      src={banner}
+      alt="Bannière bleue abstraite pour la page Portfolio"
+      className="w-100"/>
+
+      <Container aria-labelledby="portfolio-title">
+
+        <h1
+        id="porfolio-title"
+        className="visually-hidden">
+          Section Portfolio du site
+        </h1>
+
+        <SectionDivider
+        title="Portfolio"
+        paragraph="Voici quelques-unes de mes réalisations"
+        variant="centered"/>
+
+        <CardPorfolio/>
+      </Container>
+    </>
   )
 }
