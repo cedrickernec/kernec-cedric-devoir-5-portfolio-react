7f54c01c90d8ef3575765f1a40d4225fab6f7d50
CedricKernec <cedrickernec.ck@gmail.com>
2025-11-19 12:12:24 +0100

    fix : ajout de aria-hidden pour icônes décoratives non vocalisées

--- src/components/layout/ModalGitHubItem.jsx
+++ src/components/layout/ModalGitHubItem.jsx
@@ -5,7 +5,10 @@ export default function ModalGitHubItem({icon, label, value, type, href}) {
         <div className="github-item-wrapper mb-2 pb-2">
             <div className="ps-3">
                 <p className="text-start mb-0">
-                    <i className={`${icon} github-item-icon me-2`}/>
+                    <i
+                    className={`${icon} github-item-icon me-2`}
+                    aria-hidden="true"
+                    />
                         {type === "link" ? (
                         <a
                         href={href}
